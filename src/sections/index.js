6602c93446c22c0b84b8910ad1b1785382300fbf
Nitheeshlingam <nitheeshlingam2024@gmail.com>
2025-01-16 23:35:22 +0530

    Fixed .gitignore line endings

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+import SpecialOffer from './SpecialOffer';
+import Services from './Services';
+import Footer from './Footer';
+
+export{
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    CustomerReviews,
+    Footer,
+    Subscribe,
+    SpecialOffer
+}
